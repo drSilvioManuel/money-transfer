f2cfa66e07cc7cbbb10a48b487354a45d27043f6
Silvio Manuel <dr.silvio.manuel@gmail.com>
2019-09-05 19:01:28 +0300

    The redundant class and comment have been removed

--- src/main/java/com/revolut/BalanceManager.java
+++ src/main/java/com/revolut/BalanceManager.java
@@ -18,36 +18,4 @@ public class BalanceManager {
     public static Response buildTransferResponse(int from, int to, double money) {
         return () -> AccountManager.transfer(from, to, money);
     }
-
-//    public static Response buildResponse(RequestMessage.General message) {
-//        Response response;
-//        switch (message.getOperation().type) {
-//            case DEPOSIT:
-//                response = () ->
-//                        AccountManager.deposit(
-//                                message.getId(),
-//                                message.getOperation().money);
-//                break;
-//            case WITHDRAW:
-//                response = () ->
-//                        AccountManager.withdraw(
-//                                message.getId(),
-//                                message.getOperation().money);
-//                break;
-//            case TRANSFER:
-//                RequestMessage.Transfer extendedMessage = (RequestMessage.Transfer) message;
-//
-//                response = () ->
-//                        AccountManager.transfer(
-//                                extendedMessage.getId(),
-//                                extendedMessage.getIdTo(),
-//                                extendedMessage.getOperation().money);
-//                break;
-//            default:
-//                response = () -> {
-//                    throw new InvalidOperationException();
-//                };
-//        }
-//        return response;
-//    }
 }
